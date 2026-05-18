f044056ffaff027313dba870cfe2fcd98a52d89b
oOBobbyOo <944831575@qq.com>
2026-05-18 15:29:23 +0800

    refactor: butter-auth protection

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,8 @@
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+
+import { auth } from '@/lib/auth'
 
 /**
  * TODO: 判断 Token 不存在或过期
@@ -12,17 +15,26 @@ function isTokenExpired() {
   return false
 }
 
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
 
   console.log('[ pathname ] >>:', pathname)
   console.log('[ request ] >>:', request)
 
-  // 跳转登录页携带重定向地址
+  // 自定义验证Token, 跳转登录页携带重定向地址
   if (isTokenExpired()) {
     const authUrl = new URL('/login', request.url)
     authUrl.searchParams.set('redirect', request.url)
     return NextResponse.redirect(authUrl)
+  }
+
+  // Better Auth Protection
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
   return NextResponse.next()
