5c2d9c6cb362c6f7d039a5ff58644aecde7b936b
oOBobbyOo <944831575@qq.com>
2026-05-18 16:31:28 +0800

    refactor: add Skeleton

--- app/rankings/page.tsx
+++ app/rankings/page.tsx
@@ -2,6 +2,7 @@
 
 import { useParams } from 'next/navigation'
 
+import { Skeleton } from '@/components/ui/skeleton'
 import { useRankings, VALID_PERIODS } from '@/services/ranking'
 
 export default function RankingsPage() {
@@ -14,6 +15,15 @@ export default function RankingsPage() {
   console.log(data)
   console.log(isLoading)
   console.log(error)
+
+  if (isLoading)
+    return (
+      <div className="space-y-6">
+        <Skeleton className="h-[420px] w-full rounded-xl" />
+        <Skeleton className="h-[360px] w-full rounded-xl" />
+        <Skeleton className="h-[180px] w-full rounded-xl" />
+      </div>
+    )
 
   return <div>Rankings</div>
 }
