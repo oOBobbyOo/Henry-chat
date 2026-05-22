b7996f28aec8b6bc1cd8abc8facc241913a88887
oOBobbyOo <944831575@qq.com>
2026-05-22 15:30:20 +0800

    refactor: time

--- lib/time.ts
+++ lib/time.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs'
+import { dayjs } from './dayjs'
 
 export function formatTime(date: Date): string {
   const d = dayjs(date)
@@ -16,4 +16,22 @@ export function formatTime(date: Date): string {
 
   // 跨年显示 年-月-日 时:分
   return d.format('YYYY-MM-DD HH:mm')
+}
+
+/**
+ * Format date to string
+ * @param date - Unix timestamp (seconds), date string, or Date object
+ * @param formatStr - dayjs format string, default 'YYYY-MM-DD'
+ */
+export function formatDate(date: number | string | Date, formatStr: string = 'YYYY-MM-DD'): string {
+  let d: dayjs.Dayjs
+
+  // 如果是数字且为秒级时间戳（小于 10 位数），转为毫秒
+  if (typeof date === 'number') {
+    d = date < 1e10 ? dayjs(date * 1000) : dayjs(date)
+  } else {
+    d = dayjs(date)
+  }
+
+  return d.format(formatStr)
 }
